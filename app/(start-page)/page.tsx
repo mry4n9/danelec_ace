85cb5adc68cca65cb9756f8660dac9428f76d874
mry4n9 <mr.y4n9@gmail.com>
2025-09-18 14:45:01 +0200

    Changed landing page color
    Changed landing page color and design.
    
    Added favicon.
    
    Tried some text effects.

--- app/(start-page)/page.tsx
+++ app/(start-page)/page.tsx
@@ -5,18 +5,29 @@ import Link from "next/link";
 export default function LandingPage() {
   return (
     <>
-      <h1 className="font-mono">Welcome to Danelec ACE</h1>
+      <h1 className="font-mono mb-6 text-balance">
+        Welcome to Danelec
+        <span className="italic text-[#FF4E2A]">ACE</span>
+      </h1>
 
-      <p>
-        Create ready-to-post campaigns in minutes. Danelec AI Content Engine
-        (ACE) streamlines the entire process with a guided, step-by-step
-        workflow - brining everything into a single app so you can focus on
-        strategy, not copy-pasting.
+      <p className="text-lg  mb-10 max-w-3xl mx-auto leading-relaxed text-muted-foreground">
+        Danelec's in-house campaign tool allows you to create ready-to-post
+        campaigns in minutes.
+        <span className="text-foreground"> Danelec</span>
+        <span className="italic text-foreground">ACE</span>{" "}
+        <span className="text-foreground">(AI Content Engine)</span> streamlines
+        the entire process with a{" "}
+        <span className="text-foreground">guided, step-by-step workflow</span> -
+        brining everything into a single app so you can focus on{" "}
+        <span className="text-foreground">strategy</span>, not copy-pasting.
       </p>
 
-      <Button asChild className="inline-flex items-center gap-2 font-mono">
+      <Button
+        asChild
+        className="inline-flex items-center gap-2 font-mono text-white border border-transparent dark:bg-neutral-800 "
+      >
         <Link href="/wizard">
-          <Atom className="size-6 text-teal-300 dark:text-teal-500" />
+          <Atom className="size-6 text-[#FF4E2A]" />
           Get Started
           <NavArrowRight />
         </Link>
--- app/editor/components/ad-card.tsx
+++ app/editor/components/ad-card.tsx
@@ -10,7 +10,7 @@ export default function AdCard() {
     <div>
       <Card>
         <CardHeader></CardHeader>
-
+        
         <CardContent></CardContent>
 
         <CardFooter></CardFooter>
