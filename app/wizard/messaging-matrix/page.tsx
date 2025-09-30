6a1fa980d97f349b246ec2ba5e729cb968a2ebce
mry4n9 <mr.y4n9@gmail.com>
2025-09-30 09:33:56 +0200

    added anonymous function

--- app/wizard/messaging-matrix/page.tsx
+++ app/wizard/messaging-matrix/page.tsx
@@ -1,5 +1,3 @@
-export default function (){
-    return (
-        <div>Select matrix</div>
-    )
+export default function MatrixSelector() {
+  return <div>Select matrix</div>;
 }
--- app/wizard/solution/page.tsx
+++ app/wizard/solution/page.tsx
@@ -1,4 +1,4 @@
-export default function (){
+export default function SolutionSelector(){
     return (
         <div>Select solution</div>
     )
